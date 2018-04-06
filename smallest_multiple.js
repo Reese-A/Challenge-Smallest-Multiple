40e61a5b2c5694b09ca51f1e7f9353e1e4c856be
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-04-06 10:10:05 -1000

    first commit

--- smallest_multiple.js
+++ smallest_multiple.js
@@ -6,9 +6,23 @@
  * @return { Number }         Lowest Positive Number that is evenly divisible by all numbers
  *                            between 1 and `ceiling`
  */
-module.exports = function( ceiling ) {
+module.exports = function ( ceiling ) {
   // do work here
-
-
-  return 0;
+  let dividends = [];
+  for(let i=0;i<=ceiling; i++){
+    dividends.push[i];
+    }
+  let result = ceiling;
+  let counter = 0;
+  while(counter<result){
+    for(let j=0;j<dividends.length;j++){
+      if(result%dividends[j] === 0){
+        counter +=1;
+      }else{
+        counter = 0;
+        result += 1;
+      }
+    }
+  }
+  return result;
 };
